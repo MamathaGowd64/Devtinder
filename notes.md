05f761d97bcad5bf4e52048b4c989b390cfaddca
MamathaGowd64 <mamatha.gowd2020@gmail.com>
2024-10-05 10:18:12 +0530

    handlig routes using next()

--- notes.md
+++ notes.md
@@ -11,4 +11,6 @@
 -order of the routes matters a lot
 -install postman to test the apis
 -regular expressions in routes ((ab)?(means ab is optional),b+(we can add many bs),(ab*cd)*(means in between we can use anything))
+-app.use(we can use all methods get,delete,post,put)
+-multiple routes can by handled by next()
 
--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.get("/u*ser", (req, res) => {
-    res.send("mamatha")
-})
+app.get("/user", (req, res, next) => {
+    console.log("1st response")
+    //res.send("1");
+    next();
+}, (req, res, next) => {
+    console.log("2nd response")
+    //res.send("2");
+    next();
+    console.log("hi")
+},(req, res, next) => {
+    console.log("3 response")
+   res.send("3");
+    //next();
+},(req, res, next) => {
+    console.log("4 response")
+   // res.send("4");
+  // next();
+},)
 app.listen(5000, () => {
     console.log('server listening on 5000');
 })
